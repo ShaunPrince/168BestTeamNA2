81185387ed16e20051a569f15c1faabbf9488d2b
Shaun <prince.a.shaun@gmail.com>
2019-06-04 11:07:00 -0700

    Made 5 piece prefabs, made new floor with row and block prefabs, added killbox script, fixed piece spawning

--- BreakTheIce/Assets/_Scripts/Polar Bear/BearController.cs
+++ BreakTheIce/Assets/_Scripts/Polar Bear/BearController.cs
@@ -24,7 +24,7 @@ public class BearController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnPiece();
     }
 
     // Update is called once per frame
--- BreakTheIce/Assets/_Scripts/Polar Bear/Piece.cs
+++ BreakTheIce/Assets/_Scripts/Polar Bear/Piece.cs
@@ -43,4 +43,9 @@ public class Piece : MonoBehaviour
         }
     }
 
+    private void OnDestroy()
+    {
+        
+    }
+
 }
